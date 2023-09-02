6259e043f16e96c433e7aab47b404275315b5fdb
Juanmaishere <rsjuanmanuel@hotmail.com>
2023-09-02 13:57:58 +0000

    feat

--- main.c
+++ main.c
@@ -1,24 +1,20 @@
 #include "monty.h"
 
-data_t dat; // Declare dat as a global variable to store interpreter data
+data_t dat;
 
-// Function to push an element onto the stack
 void _push(stack_t **stack, unsigned int line_number)
 {
     int c = 0;
     stack_t *node;
 
-    // Check for missing value
     if (!dat.tokens[1])
     {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    // Convert token to integer
     c = atoi(dat.tokens[1]);
 
-    // Create a new stack node
     node = malloc(sizeof(stack_t));
     if (!node)
     {
@@ -35,9 +31,8 @@ void _push(stack_t **stack, unsigned int line_number)
 
     *stack = node;
 }
-
-// Function to print all elements in the stack
-void _pall(stack_t **stack, __attribute__((unused))unsigned int line_number)
+void
+_pall(stack_t **stack, __attribute__((unused))unsigned int line_number)
 {
     stack_t *temp = *stack;
 
@@ -47,16 +42,14 @@ void _pall(stack_t **stack, __attribute__((unused))unsigned int line_number)
         temp = temp->next;
     }
 }
-
-// Function for a no-op instruction
-void nop_f(stack_t **stack, unsigned int line_number)
+void
+nop_f(stack_t **stack, unsigned int line_number)
 {
     (void)stack;
     (void)line_number;
 }
-
-// Function to get the corresponding function for an opcode
-void (*getfunc(char *op))(stack_t **stack, unsigned int line_number)
+void
+(*getfunc(char *op))(stack_t **stack, unsigned int line_number)
 {
     int i = 0;
 
@@ -82,7 +75,7 @@ void (*getfunc(char *op))(stack_t **stack, unsigned int line_number)
 
 int main(int argc, char *argv[])
 {
-    int line_number = 0; // Initialize line_number
+    int line_number = 0;
     FILE *file;
     char *line = NULL;
     size_t len = 0;
@@ -105,7 +98,6 @@ int main(int argc, char *argv[])
     {
         line_number++;
 
-        // Tokenize the input line
         int i = 0;
         char *tokens[1000], *tok;
         tok = strtok(line, " \t\n");
@@ -116,11 +108,9 @@ int main(int argc, char *argv[])
             i++;
         }
 
-        // Set tokens in the data structure for use in getfunc
         dat.tokens[0] = tokens[0];
         dat.tokens[1] = tokens[1];
 
-        // Get the function corresponding to the opcode and execute it
         void (*instruction_func)(stack_t **, unsigned int) = getfunc(dat.tokens[0]);
         if (instruction_func)
         {
@@ -132,7 +122,6 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
 
-        // Free allocated tokens
         for (int j = 0; j < i; j++)
         {
             free(tokens[j]);
@@ -144,7 +133,6 @@ int main(int argc, char *argv[])
 
     fclose(file);
 
-    // Free remaining stack nodes
     while (dat.top != NULL)
     {
         stack_t *temp = dat.top;
